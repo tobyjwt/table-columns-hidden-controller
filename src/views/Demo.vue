7920e9c0be73ecedc3d549d7acd6c6346b90d3be
jinwentao <jinwentao@bigo.sg>
2021-09-16 16:34:24 +0800

    完善demo

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -12,13 +12,30 @@
       :data="tableData"
       :checked="tableChecked"
       border
+      @selection-change="handleSelectionChange"
     >
+      <el-table-column
+        type="selection"
+        width="55"
+        align="center"
+        fixed
+      />
+
       <el-table-column
         v-for="item in columns"
         :key="item.key"
         :label="item.label"
         align="center"
         :prop="item.key"/>
+
+      <el-table-column
+        label="操作"
+        align="center"
+      >
+        <template>
+          <el-button type="text">按钮</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -46,6 +63,11 @@ export default {
     }
     for (let i = 0; i < 9; i++) {
       this.tableData.push({ ...data })
+    }
+  },
+  methods: {
+    handleSelectionChange (val) {
+      console.log(val)
     }
   }
 }
